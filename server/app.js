b14695110f026a64090fb4b4bc7e9c473a429670
clarkdowner <cddowner@gmail.com>
2017-01-24 15:39:14 -0800

    Basic routes added

--- server/app.js
+++ server/app.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.send('This isn\'t Reddit');
 })
 
-app.listen(3000, function () {
+app.get('/hot', function(req, res) {
+	res.send('These posts are hot');
+})
+
+app.get('/top', function(req, res) {
+	res.send('These are the top posts');
+})
+
+app.get('/saved', function(req, res) {
+	res.send('These are my saved posts');
+})
+
+app.post('/saved', function(req, res) {
+	res.send('Eventually this will save a post');
+})
+
+app.listen(3000, function() {
   console.log('Listening on 3000');
 })
